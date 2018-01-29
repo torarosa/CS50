5f5b3e487595f72223cbd8dea6aa2311488b644c
Dair Graycen <nautilusdranus@gmail.com>
2018-01-29 10:21:27 +0600

    Importing templates & printing values

--- similarities_go/main.go
+++ similarities_go/main.go
@@ -1,33 +1,47 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
+	"encoding/json"
 	"log"
 	"net/http"
+	"os"
+	"templating"
 )
 
-var tpl *template.Template
-
-func init() {
-	tpl = template.Must(template.ParseGlob("assets/templates/*.tmpl"))
+type Configuration struct {
+	LayoutPath  string
+	IncludePath string
 }
 
-func GetStrings(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	t, err := tpl.ParseFiles("assets/templates/output.tmpl")
+func loadConfiguration(fileName string) {
+	file, _ := os.Open(fileName)
+	decoder := json.NewDecoder(file)
+	configuration := Configuration{}
+	err := decoder.Decode(&configuration)
 	if err != nil {
-		log.Fatalln(err)
+		log.Println("error:", err)
 	}
-	firstString := r.FormValue("firstString")
-	secondString := r.FormValue("secondString")
-	defer t.Execute(w, nil)
-	fmt.Println("First:", firstString)
-	fmt.Println("Second:", secondString)
+	log.Println("layout path: ", configuration.LayoutPath)
+	log.Println("include path: ", configuration.IncludePath)
+	templating.SetTemplateConfig(configuration.LayoutPath, configuration.IncludePath)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	var g [2]string
+	if r.Method == http.MethodPost {
+		firstString := r.FormValue("firstString")
+		secondString := r.FormValue("secondString")
+		g = [2]string{firstString, secondString}
+	}
+
+	templating.RenderTemplate(w, "index.tmpl", g)
 }
 
 func main() {
-	http.HandleFunc("/", GetStrings)
+	loadConfiguration("config.json")
+	templating.LoadTemplates()
+	http.HandleFunc("/", index)
 	http.ListenAndServe(":8080", nil)
-
 }
